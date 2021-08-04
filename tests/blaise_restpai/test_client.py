b33f115c51df29489b066dc260c4e3d689f65f72
jamie kerr <jamiestephenkerr@gmail.com>
2021-08-04 09:06:08 +0100

    moved test response to stub

--- tests/blaise_restpai/test_client.py
+++ tests/blaise_restpai/test_client.py
@@ -1,13 +1,14 @@
 import responses
 
+from blaise_restapi.stubs.instrument_stubs import api_questionnaires_stub_response
 
 @responses.activate
-def test_get_all_questionnaires_from_server_park(client, server_park, api_questionnaires_response):
+def test_get_all_questionnaires_from_server_park(client, server_park):
     responses.add(
         responses.GET,
         f"http://localhost/api/v1/serverparks/{server_park}/instruments",
-        json=api_questionnaires_response,)
+        json=api_questionnaires_stub_response(),)
 
     result = client.get_all_questionnaires_from_server_park(server_park)
-    assert result == api_questionnaires_response
+    assert result == api_questionnaires_stub_response()
     assert len(result) == 3
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,85 +2,12 @@ import pytest
 
 from blaise_restapi.client import Client
 
+
 @pytest.fixture
 def client():
     return Client("http://localhost")
 
+
 @pytest.fixture
 def server_park():
     return "gusty"
-
-
-@pytest.fixture
-def api_questionnaires_response():
-    return [
-        {
-            "name": "DST2106X",
-            "id": "12345-12345-12345-12345-XXXXX",
-            "serverParkName": "gusty",
-            "installDate": "2021-01-01T01:01:01.99999+01:00",
-            "status": "Active",
-            "dataRecordCount": 1337,
-            "hasData": True,
-            "nodes": [
-                {
-                    "nodeName": "blaise-gusty-mgmt",
-                    "nodeStatus": "Active"
-                },
-                {
-                    "nodeName": "blaise-gusty-data-entry-1",
-                    "nodeStatus": "Active"
-                },
-                {
-                    "nodeName": "blaise-gusty-data-entry-2",
-                    "nodeStatus": "Active"
-                }
-            ]
-        },
-        {
-            "name": "DST2106Y",
-            "id": "12345-12345-12345-12345-YYYYY",
-            "serverParkName": "gusty",
-            "installDate": "2021-01-01T01:01:01.99999+01:00",
-            "status": "Active",
-            "dataRecordCount": 42,
-            "hasData": True,
-            "nodes": [
-                {
-                    "nodeName": "blaise-gusty-mgmt",
-                    "nodeStatus": "Active"
-                },
-                {
-                    "nodeName": "blaise-gusty-data-entry-1",
-                    "nodeStatus": "Active"
-                },
-                {
-                    "nodeName": "blaise-gusty-data-entry-2",
-                    "nodeStatus": "Active"
-                }
-            ]
-        },
-        {
-            "name": "DST2106Z",
-            "id": "12345-12345-12345-12345-ZZZZZ",
-            "serverParkName": "gusty",
-            "installDate": "2021-01-01T01:01:01.99999+01:00",
-            "status": "Active",
-            "dataRecordCount": 999,
-            "hasData": True,
-            "nodes": [
-                {
-                    "nodeName": "blaise-gusty-mgmt",
-                    "nodeStatus": "Active"
-                },
-                {
-                    "nodeName": "blaise-gusty-data-entry-1",
-                    "nodeStatus": "Active"
-                },
-                {
-                    "nodeName": "blaise-gusty-data-entry-2",
-                    "nodeStatus": "Active"
-                }
-            ]
-        }
-    ]
